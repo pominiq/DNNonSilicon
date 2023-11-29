435942acda596a8ecefa81fade691106890bd869
pominiq <davidmatthewlane@gmail.com>
2023-11-29 14:25:32 +0100

    updated model and openlane script

--- AYAM_3_Openlane2-flow.py
+++ AYAM_3_Openlane2-flow.py
@@ -19,7 +19,7 @@ and Netgen for generating netlists. Additionally Klayout can be used to view the
 import os
 from openlane.flows import SequentialFlow, Flow
 from openlane.steps import Yosys, Misc, OpenROAD, Magic, Netgen
-
+from openlane.steps.openroad import OpenGUI
 
 class MyFlow1(SequentialFlow):
     Steps = [
@@ -32,6 +32,7 @@ class MyFlow1(SequentialFlow):
         OpenROAD.GlobalPlacement,
         OpenROAD.DetailedPlacement,
         OpenROAD.GlobalRouting,
+        #OpenROAD.CheckAntennas,
         OpenROAD.DetailedRouting,
         OpenROAD.FillInsertion,
         Magic.StreamOut,
@@ -58,21 +59,41 @@ def main():
         {
             "PDK": "sky130A",
             "DESIGN_NAME": "myproject",
-            "VERILOG_FILES": "refg::$DESIGN_DIR/model_1/hls4ml_prj/myproject_prj/solution1/impl/verilog/*.v",
+            "VERILOG_FILES": "refg::$DESIGN_DIR/model_4/hls4ml_prj/myproject_prj/solution1/impl/verilog/*.v",
             "CLOCK_PORT": 'ap_clk',
             "CLOCK_PERIOD": 10,
             "USE_LIGHTER": True,
             "SYNTH_AUTONAME": False,
             "SYNTH_NO_FLAT": True,
             "SYNTH_SIZING": True,
+            "SYNTH_ABC_BUFFERING": False,
+            "SYNTH_SHARE_RESOURCES": True,
             "SYNTH_STRATEGY": "DELAY 4",
+            "MAX_TRANSITION_CONSTRAINT": 1.0,
+            "MAX_FANOUT_CONSTRAINT": 16,
             "FP_SIZING": "absolute",
-            "DIE_AREA": [0, 0, 5000, 5000],
+            "DIE_AREA": [0, 0, 100000, 100000],
+            "PL_RESIZER_SETUP_SLACK_MARGIN": 0.4,
+            "GRT_RESIZER_SETUP_SLACK_MARGIN": 0.2,
+            "GRT_RESIZER_HOLD_SLACK_MARGIN": 0.2,
+            "PL_RESIZER_HOLD_SLACK_MARGIN": 0.4,
+            "MAGIC_DEF_LABELS": False,
+            "RUN_ANTENNA_REPAIR": False,
+            "RUN_HEURISTIC_DIODE_INSERTION": False,
+            #"HEURISTIC_ANTENNA_THRESHOLD": 110,
+            "GRT_ANTENNA_ITERS": 5,
+            "GRT_ADJUSTMENT": 0.2,
+            "PL_TARGET_DENSITY_PCT": 55,
+            "PL_MAX_DISPLACEMENT_X": 500,
+            "PL_MAX_DISPLACEMENT_Y": 500,
+            "PL_ROUTABILITY_DRIVEN": True,
+            #"PL_MACRO_HALO": "30 30",
+            #"PL_MACRO_CHANNEL": "30 30",
             "FP_PDN_VPITCH": 30,
             "FP_PDN_HPITCH": 30,
             "FP_PDN_VOFFSET": 5,
             "FP_PDN_HOFFSET": 5,
-            "FP_PDN_SKIPTRIM": True,
+            "FP_PDN_SKIPTRIM": False,
         },
         design_dir=".",
     )
